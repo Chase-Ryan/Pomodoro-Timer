72ec1a6bf7954cf5da0d81b28f497ed573085b9a
Chase Ryan <89358230+Chase-Ryan@users.noreply.github.com>
2021-11-04 10:21:10 -0500

    Remove comments

--- src/pomodoro/Session.js
+++ src/pomodoro/Session.js
@@ -6,17 +6,13 @@ export default function Session({ session, currentDuration }) {
 
   if (!session) return null;
 
-  // TODO: This area should show only when there is an active focus or break - i.e. the session is running or is paused
   return (
     <div>
-      {/* TODO: This area should show only when there is an active focus or break - i.e. the session is running or is paused */}
       <div className="row mb-2">
         <div className="col">
-          {/* TODO: Update message below to include current session (Focusing or On Break) total duration */}
           <h2 data-testid="session-title">
             {session?.label} for {minutesToDuration(currentDuration)} minutes
           </h2>
-          {/* TODO: Update message below correctly format the time remaining in the current session */}
           <p className="lead" data-testid="session-sub-title">
            {secondsToDuration(session?.timeRemaining)} remaining
           </p>
